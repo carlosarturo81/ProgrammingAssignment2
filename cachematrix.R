c4d67560c6061f97c0b35313dccbea68dd490afc
carlosarturo81 <carlosarturo81@gmail.com>
2020-12-19 10:23:00 -0500

    inverse of a square invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix puts an invertible square matrix in a list with the command solve, 
+## to calculate the inverse of a matrix with the function cacheSolve 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invm <- NULL
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) invm <<- solve
+  getsolve <- function() invm
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve obtains the inverse of a invertible square matrix and storage it in a 
+## variable invm, only if invm variable is Null.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invm <- x$getsolve()
+  if(!is.null(invm)) {
+    message("getting cached data")
+    return(invm)
+  }
+  data <- x$get()
+  invm <- solve(data, ...)
+  x$setsolve(invm)
+  invm
 }
